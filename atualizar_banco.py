7cbdd50cd81d689778f29c158bd79020b143f870
Wallace <167040540+fireprojectx@users.noreply.github.com>
2025-06-06 11:13:28 -0300

    Update atualizar_banco.py

--- atualizar_banco.py
+++ atualizar_banco.py
@@ -27,8 +27,31 @@ conn.commit()
 urls = {
     ("Menor de 1 ano", "Masculino"): "http://tabnet.saude.mg.gov.br/csv/A13132110_14_8_2.csv",
     ("1 a 4 anos", "Masculino"): "http://tabnet.saude.mg.gov.br/csv/A13130210_14_8_2.csv",
+    ("5 a 9 anos", "Masculino"): "http://tabnet.saude.mg.gov.br/csv/A13123610_14_8_2.csv",
+    ("10 a 14 anos", "Masculino"): "http://tabnet.saude.mg.gov.br/csv/A13121610_14_8_2.csv",
+    ("15 a 19 anos", "Masculino"): "http://tabnet.saude.mg.gov.br/csv/A16022510_14_8_2.csv",
+    ("20 a 29 anos", "Masculino"): "http://tabnet.saude.mg.gov.br/csv/A13110610_14_8_2.csv",
+    ("30 a 39 anos", "Masculino"): "http://tabnet.saude.mg.gov.br/csv/A13103710_14_8_2.csv",
+    ("40 a 49 anos", "Masculino"): "http://tabnet.saude.mg.gov.br/csv/A13101310_14_8_2.csv",
+    ("50 a 59 anos", "Masculino"): "http://tabnet.saude.mg.gov.br/csv/A13093410_14_8_2.csv",
+    ("60 a 69 anos", "Masculino"): "http://tabnet.saude.mg.gov.br/csv/A13090210_14_8_2.csv",
+    ("70 a 79 anos", "Masculino"): "http://tabnet.saude.mg.gov.br/csv/A13054010_14_8_2.csv",
+    ("80 e mais", "Masculino"): "http://tabnet.saude.mg.gov.br/csv/A13052310_14_8_2.csv",
+    ("Idade ignorada", "Masculino"): "http://tabnet.saude.mg.gov.br/csv/A13043210_14_8_2.csv",
+
     ("Menor de 1 ano", "Feminino"): "http://tabnet.saude.mg.gov.br/csv/A13335310_14_8_2.csv",
     ("1 a 4 anos", "Feminino"): "http://tabnet.saude.mg.gov.br/csv/A13342210_14_8_2.csv",
+    ("5 a 9 anos", "Feminino"): "http://tabnet.saude.mg.gov.br/csv/A13355610_14_8_2.csv",
+    ("10 a 14 anos", "Feminino"): "http://tabnet.saude.mg.gov.br/csv/A13362210_14_8_2.csv",
+    ("15 a 19 anos", "Feminino"): "http://tabnet.saude.mg.gov.br/csv/A13370510_14_8_2.csv",
+    ("20 a 29 anos", "Feminino"): "http://tabnet.saude.mg.gov.br/csv/A13372010_14_8_2.csv",
+    ("30 a 39 anos", "Feminino"): "http://tabnet.saude.mg.gov.br/csv/A13373810_14_8_2.csv",
+    ("40 a 49 anos", "Feminino"): "http://tabnet.saude.mg.gov.br/csv/A13375810_14_8_2.csv",
+    ("50 a 59 anos", "Feminino"): "http://tabnet.saude.mg.gov.br/csv/A13381610_14_8_2.csv",
+    ("60 a 69 anos", "Feminino"): "http://tabnet.saude.mg.gov.br/csv/A13383210_14_8_2.csv",
+    ("70 a 79 anos", "Feminino"): "http://tabnet.saude.mg.gov.br/csv/A13385110_14_8_2.csv",
+    ("80 e mais", "Feminino"): "http://tabnet.saude.mg.gov.br/csv/A13390510_14_8_2.csv",
+    ("Idade ignorada", "Feminino"): "http://tabnet.saude.mg.gov.br/csv/A13392110_14_8_2.csv",
 }
 
 # Processamento
